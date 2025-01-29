f1c881b3ead4b9685416c9eeb1b988c76a8c5a3f
Nick Hayward <info@fcswebsites.co.uk>
2025-01-29 23:43:55 +0000

    ajax search refinement

--- functions.php
+++ functions.php
@@ -121,7 +121,7 @@ function data_fetch() {
 	$the_query = new WP_Query(
 		array(
 			'posts_per_page' => -1,
-			// 's'              => esc_attr( $_POST['keyword'] ),
+			's'              => esc_attr( $_POST['keyword'] ),
 			'post_type'      => array( 'epkb_post_type_1' ),
 		)
 	);
